b8576e65f850a600d31ca061aa5b55140018ebef
evelo <evelo0702@gmail.com>
2023-05-15 17:22:15 +0900

    feat useReducer, context 를 활용한 최적화

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useReducer, useRef } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
@@ -9,54 +10,111 @@ import Diary from "./pages/Diary";
 import Button from "./components/Button";
 import Header from "./components/Header";
 
+const reducer = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      newState = [action.data, ...state];
+      break;
+    }
+    case "REMOVE": {
+      newState = state.filter((it) => it.id !== action.targetId);
+      break;
+    }
+    case "EDIT": {
+      newState = state.map((it) =>
+        it.id === action.data.id ? { ...action.data } : it
+      );
+      break;
+    }
+    default:
+      return state;
+  }
+  return newState;
+};
+// context
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContenxt = React.createContext();
+
 function App() {
+  // 기본경로설정
   const env = process.env;
   env.PUBLIC_URL = env.PUBLIC_URL || "";
 
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Header
-          headText={"App"}
-          leftchild={
-            <Button
-              text={"왼쪽버튼"}
-              onClick={() => {
-                alert("왼쪽 클릭");
-              }}
-            />
-          }
-          rightchild={
-            <Button
-              text={"오른쪽버튼"}
-              onClick={() => {
-                alert("오른쪽 클릭");
-              }}
-            />
-          }
-        />
-        <h2>App.js</h2>
-        <Button
-          text={"버튼"}
-          onClick={() => alert("버튼클릭")}
-          type={"positive"}
-        />
-        <Button
-          text={"버튼"}
-          onClick={() => alert("버튼클릭")}
-          type={"negative"}
-        />
-        <Button text={"버튼"} onClick={() => alert("버튼클릭")} />
+  const [data, dispatch] = useReducer(reducer, []);
+  const dataId = useRef(0);
+  // CREATE
+  const onCreate = (date, content, emotion) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+    dataId.current += 1;
+  };
+  // REMOVE
+  const onRemove = (targetId) => {
+    dispatch({
+      type: "REMOVE",
+      targetId,
+    });
+  };
+  // EDIT
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type: "EDIT",
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+  };
 
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/New" element={<New />}></Route>
-          <Route path="/Edit" element={<Edit />}></Route>
-          <Route path="/Diary/:id" element={<Diary />}></Route>
-          {/* <Route path="/Diary/" element={<Diary />}></Route> */}
-        </Routes>
-      </div>
-    </BrowserRouter>
+  return (
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContenxt.Provider value={{ onCreate, onEdit, onRemove }}>
+        <BrowserRouter>
+          <div className="App">
+            <Header
+              headText={"App"}
+              leftchild={
+                <Button
+                  text={"왼쪽버튼"}
+                  onClick={() => {
+                    alert("왼쪽 클릭");
+                  }}
+                />
+              }
+              rightchild={
+                <Button
+                  text={"오른쪽버튼"}
+                  onClick={() => {
+                    alert("오른쪽 클릭");
+                  }}
+                />
+              }
+            ></Header>
+
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/New" element={<New />}></Route>
+              <Route path="/Edit" element={<Edit />}></Route>
+              <Route path="/Diary/:id" element={<Diary />}></Route>
+              {/* <Route path="/Diary/" element={<Diary />}></Route> */}
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </DiaryDispatchContenxt.Provider>
+    </DiaryStateContext.Provider>
   );
 }
 
